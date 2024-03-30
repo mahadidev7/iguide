59ccbac62b2a31aae36d8670f32519e2c61565d4
mahadidev7 <mahadidev7@gmail.com>
2024-03-30 16:49:42 +0600

    optimize code of help in jabascript code

--- js/helpScript.js
+++ js/helpScript.js
@@ -14,16 +14,20 @@ function closeMenuHandler() {
 let accordionBodyGroup = document.querySelectorAll(
   ".AccordionForHelp .accordionBodyGroup"
 );
-let accordionItemHelp = document.querySelectorAll(
+let accordionItemsHelp = document.querySelectorAll(
   ".AccordionForHelp .accordionItem"
 );
-let downIcon = document.querySelectorAll(
-  ".AccordionForHelp .accordionItem .downIcon"
+
+let accordionHeads = document.querySelectorAll(".accordionHeader");
+let downIcons = document.querySelectorAll(
+  ".AccordionForHelp .accordionItem .downIcons"
 );
 let searchButtonStepOne = document.querySelectorAll(".searchButtonStepOne");
 let stepOneHelp = document.querySelectorAll(".stepOneHelp");
 let stepTwoHelp = document.querySelectorAll(".stepTwoHelp");
 let stepThreeHelp = document.querySelectorAll(".stepThreeHelp");
+
+let input_groups = document.querySelectorAll(".input_group");
 
 let allInput = document.querySelectorAll(".input");
 let label = document.querySelectorAll("label");
@@ -35,134 +39,87 @@ let helpFormValue = {
   lastName: "",
 };
 
-// remove all active class of help - the accordion
-function removeActiveAccordionHelp() {
-  for (const [key, value] of Object.entries(accordionBodyGroup)) {
-    value.classList.remove("active");
-  }
-  for (const [key, value] of Object.entries(downIcon)) {
-    value.style.transform = "rotate(0deg)";
-  }
-}
+accordionHeads.forEach((accordionHeadItem) => {
+  let accordionTitle = accordionHeadItem.parentElement;
+  let bodyDiv = accordionTitle.querySelector(".accordionBodyGroup");
+  let icon = accordionTitle.querySelector(".downIcon");
 
-// function accordionToggleItemHelp() {
-//   [...accordionItemHelp]?.map((item, index) => (
-//     item.addEventListener("click", function () {
-//       if(accordionBodyGroup[index].classList.contains("active")){
-//         removeActiveAccordionHelp()
-//         return
-//       }
-//       removeActiveAccordionHelp();
-//       accordionBodyGroup[index].classList.add("active");
-//       downIcon[index].style.transform = "rotate(180deg)";
-//     })
-//   ));
-// }
-
-// function accordionToggleItemHelp(itemNumber) {
-//   [...accordionItemHelp]?.map((item, index) => {
-//     if (item?.classList.contains(itemNumber)) {
-
-//       item.addEventListener("click", function () {
-//         console.log(item);
-//       })
-
-//       return
-//     }
-//     console.log("00");
-//     return;
-//   });
-// }
-
-accordionItemHelp[0]?.addEventListener("click", function () {
-  if (accordionBodyGroup[0].classList.contains("active")) {
-    removeActiveAccordionHelp();
-    return;
-  }
-  removeActiveAccordionHelp();
-  accordionBodyGroup[0].classList.add("active");
-  downIcon[0].style.transform = "rotate(180deg)";
-});
-accordionItemHelp[1]?.addEventListener("click", function () {
-  if (accordionBodyGroup[1].classList.contains("active")) {
-    removeActiveAccordionHelp();
-    return;
-  }
-  removeActiveAccordionHelp();
-  accordionBodyGroup[1].classList.add("active");
-  downIcon[1].style.transform = "rotate(180deg)";
-});
-accordionItemHelp[2]?.addEventListener("click", function () {
-  if (accordionBodyGroup[2].classList.contains("active")) {
-    removeActiveAccordionHelp();
-    return;
-  }
-  removeActiveAccordionHelp();
-  accordionBodyGroup[2].classList.add("active");
-  downIcon[2].style.transform = "rotate(180deg)";
+  accordionHeadItem?.addEventListener("click", () => {
+    if (bodyDiv.classList.contains("active")) {
+      bodyDiv.classList.remove("active");
+      icon.style.transform = "rotate(0deg)";
+    } else {
+      // remove all active class
+      const accordionTitlesWithIsOpen = document.querySelectorAll(".active");
+      accordionTitlesWithIsOpen.forEach((accordionTitleWithIsOpen) => {
+        accordionTitleWithIsOpen.classList.remove("active");
+      });
+      // remove all active icon class
+      const accordionTitleDownIcons = document.querySelectorAll(".downIcon");
+      accordionTitleDownIcons.forEach((icon) => {
+        icon.style.transform = "rotate(0deg)";
+      });
+      // add active class and add active icon class
+      bodyDiv.classList.add("active");
+      icon.style.transform = "rotate(180deg)";
+    }
+  });
 });
 
+// click the GO button then active STEP-2
 searchButtonStepOne[0]?.addEventListener("click", function () {
   stepOneHelp[0].style.display = "none";
   stepTwoHelp[0].style.display = "block";
 });
 
-// text box control handler - input 01
-allInput[0]?.addEventListener("input", (event) => {
-  helpFormValue["message"] = event.target.value;
-  allInput[0].style.border = "1px solid #ddd";
-  document.querySelectorAll(".errorMessage_messageHelp")[0].innerHTML = ` `;
+// input box validation with focus & blur & input value change
+input_groups?.forEach((element) => {
+  let InputBox = element.querySelector(".input");
+  let labelTag = element.querySelector("label");
+  let errorDiv = element.querySelector(".errorDiv");
+
+  InputBox?.addEventListener("focus", function () {
+    labelTag.style.top = "0";
+    labelTag.style.color = "#049f8d";
+    labelTag.style.background = "#fff";
+    return;
+  });
+
+  InputBox?.addEventListener("blur", function (e) {
+    if (!!helpFormValue[InputBox?.getAttribute("name")]) {
+      labelTag.style.top = "0";
+      labelTag.style.color = "#049f8d";
+      labelTag.style.background = "#fff";
+      errorDiv.innerHTML = ``;
+    } else {
+      InputBox.style.border = "1px solid #b00020";
+      labelTag.style.color = "#b00020";
+      labelTag.style.top = "25px";
+
+      errorDiv.innerHTML = `<p class="error">${InputBox?.getAttribute(
+        "errorMessage"
+      )}</p>`;
+    }
+  });
+
+  // validation when input box is change text
+  InputBox?.addEventListener("input", function (e) {
+    let value = e.target.value;
+    helpFormValue[InputBox?.getAttribute("name")] = value;
+
+    if (!!value) {
+      errorDiv.innerHTML = ``;
+      InputBox.style.border = "1px solid #ddd";
+    } else {
+      InputBox.style.border = "1px solid #b00020";
+      errorDiv.innerHTML = `<p class="error">${InputBox?.getAttribute(
+        "errorMessage"
+      )}</p>`;
+    }
+  });
 });
 
-// text box with label controller - label 01
-allInput[0]?.addEventListener("click", function () {
-  label[0].style.top = "0";
-  label[0].style.color = "#049f8d";
-  label[0].style.background = "#fff";
-});
-
-// text box control handler - input 02
-allInput[1]?.addEventListener("input", (event) => {
-  helpFormValue["email"] = event.target.value;
-  allInput[1].style.border = "1px solid #ddd";
-  document.querySelectorAll(".errorMessage_emailHelp")[0].innerHTML = ` `;
-});
-
-// text box with label controller - label 02
-allInput[1]?.addEventListener("click", function () {
-  label[1].style.top = "0";
-  label[1].style.color = "#049f8d";
-  label[1].style.background = "#fff";
-});
-
-// text box control handler - input 03
-allInput[2]?.addEventListener("input", (event) => {
-  helpFormValue["firstName"] = event.target.value;
-  allInput[2].style.border = "1px solid #ddd";
-  document.querySelectorAll(".errorMessage_firstNameHelp")[0].innerHTML = ` `;
-});
-
-// text box with label controller - label 03
-allInput[2]?.addEventListener("click", function () {
-  label[2].style.top = "0";
-  label[2].style.color = "#049f8d";
-  label[2].style.background = "#fff";
-});
-
-// text box control handler - input 04
-allInput[3]?.addEventListener("input", (event) => {
-  helpFormValue["lastName"] = event.target.value;
-  allInput[3].style.border = "1px solid #ddd";
-  document.querySelectorAll(".errorMessage_lastNameHelp")[0].innerHTML = ` `;
-});
-
-// text box with label controller - label 01
-allInput[3]?.addEventListener("click", function () {
-  label[3].style.top = "0";
-  label[3].style.color = "#049f8d";
-  label[3].style.background = "#fff";
-});
-
+// form submit of step 2
 function helpFormHandler() {
   // success
   if (
@@ -173,15 +130,10 @@ function helpFormHandler() {
   ) {
     const form = document.forms["helpForm"];
 
-    const message = form["message"].value;
-    const email = form["email"].value;
-    const firstName = form["firstName"].value;
-    const lastName = form["lastName"].value;
-
-    helpFormValue["message"] = message;
-    helpFormValue["email"] = email;
-    helpFormValue["firstName"] = firstName;
-    helpFormValue["lastName"] = lastName;
+    helpFormValue["message"] = form["message"].value;
+    helpFormValue["email"] = form["email"].value;
+    helpFormValue["firstName"] = form["firstName"].value;
+    helpFormValue["lastName"] = form["lastName"].value;
 
     stepOneHelp[0].style.display = "none";
     stepTwoHelp[0].style.display = "none";
@@ -189,45 +141,23 @@ function helpFormHandler() {
 
     return;
   }
+  // Error
+  input_groups?.forEach((element, i) => {
+    let InputBox = element.querySelector(".input");
+    let labelTag = element.querySelector("label");
+    let errorDiv = element.querySelector(".errorDiv");
 
-  // error message
-  if (!!!helpFormValue?.message) {
-    document.querySelectorAll(
-      ".input.PleaseDescribeYourRequest"
-    )[0].style.border = "1px solid #b00020";
-    label[0].style.color = "#b00020";
-    label[0].style.top = "25px";
-    document.querySelectorAll(
-      ".errorMessage_messageHelp"
-    )[0].innerHTML = `<p class="error">this input box is required.</p>`;
-  }
-
-  if (!!!helpFormValue?.email) {
-    allInput[1].style.border = "1px solid #b00020";
-    label[1].style.color = "#b00020";
-    label[1].style.top = "25px";
-    document.querySelectorAll(
-      ".errorMessage_emailHelp"
-    )[0].innerHTML = `<p class="error">Email is required.</p>`;
-  }
-
-  if (!!!helpFormValue?.firstName) {
-    allInput[2].style.border = "1px solid #b00020";
-    label[2].style.color = "#b00020";
-    label[2].style.top = "25px";
-    document.querySelectorAll(
-      ".errorMessage_firstNameHelp"
-    )[0].innerHTML = `<p class="error">First Name is required.</p>`;
-  }
-
-  if (!!!helpFormValue?.lastName) {
-    allInput[3].style.border = "1px solid #b00020";
-    label[3].style.color = "#b00020";
-    label[3].style.top = "25px";
-    document.querySelectorAll(
-      ".errorMessage_lastNameHelp"
-    )[0].innerHTML = `<p class="error">Last Name is required.</p>`;
-  }
+    if (!!InputBox.value) {
+      return;
+    } else {
+      InputBox.style.border = "1px solid #b00020";
+      labelTag.style.color = "#b00020";
+      labelTag.style.top = "25px";
+      errorDiv.innerHTML = `<p class="error">${InputBox?.getAttribute(
+        "errorMessage"
+      )}</p>`;
+    }
+  });
 
   return;
 }
