e723b0e2022aef7a1429b8f6a6fa019145c7baaa
mahadidev7 <mahadidev7@gmail.com>
2024-03-30 17:31:27 +0600

    working contact page...

--- js/mahadidev7.js
+++ js/mahadidev7.js
@@ -1,5 +1,5 @@
-let mahadidev7_accordingContext = document.getElementsByClassName(
-  "mahadidev7_accordingContext"
+let accordingBodyContent = document.getElementsByClassName(
+  "accordingBodyContent"
 );
 let step_one = document.querySelectorAll(".step_one");
 let step_two = document.querySelectorAll(".step_two");
@@ -20,13 +20,14 @@ let step_fourItem = document.querySelectorAll(".step_fourItem")[0];
 let ActiveSubmit = document.querySelectorAll(".ActiveSubmit")[0];
 let inactiveSubmit = document.querySelectorAll(".inactiveSubmit")[0];
 
+let input_groups = document.querySelectorAll(".input_group");
+
 let label = document.querySelectorAll("label");
 
 let allInput = document.querySelectorAll(".input");
 let arrayOfInput = [...allInput];
 
 // mobile menu berger code is hare
-
 // menu open handler
 function openMenuHandler() {
   mobileMenu.style.display = "block";
@@ -38,7 +39,7 @@ function closeMenuHandler() {
 
 // here is according open & close javascript code and form handler functions
 
-// hare is contact page all value
+// hare is contact page all form value
 let contactPageAllFormValues = {
   StartByDescribingYourIssue: "", //Start by describing your issue
   ChooseTheBestDescriptionOfYourIssue: "", // Choose the best description of your issue
@@ -50,7 +51,7 @@ let contactPageAllFormValues = {
 
 // all according item close
 let removeAllActiveClass = () => {
-  for (const [key, value] of Object.entries(mahadidev7_accordingContext)) {
+  for (const [key, value] of Object.entries(accordingBodyContent)) {
     value.classList.remove("active");
   }
 };
@@ -80,18 +81,13 @@ function removeAllValueInnerText() {
 function stepOneHandler() {
   if (!!contactPageAllFormValues?.StartByDescribingYourIssue) {
     const form = document.forms["stepOneForm"];
-    const value = form["message"].value;
-    contactPageAllFormValues["StartByDescribingYourIssue"] = value;
+
+    contactPageAllFormValues["StartByDescribingYourIssue"] = form["StartByDescribingYourIssue"].value;
+
     removeAllActiveClass();
     step_two[0].classList.add("active");
 
-    //   document.querySelectorAll(".step_oneHead .resultQuestion")[0].innerText =
-    //     "Start by describing your issue";
-    //   document.querySelectorAll(".step_oneHead .resultAnswer")[0].innerText = value;
 
-    //   document.querySelectorAll(".editGroup")[0].innerHTML = `<div class="edit">
-    //   <i class="fa-solid fa-pen-to-square"></i>
-    // </div>`;
 
     document.querySelectorAll(".step_oneHead")[0].style.display = "flex";
     document.querySelectorAll(".step_twoHead")[0].style.display = "none";
@@ -253,118 +249,124 @@ window.addEventListener("load", function () {
   return;
 });
 
-// step_oneItem.addEventListener("click", function () {
-//   if (!!contactPageAllFormValues?.StartByDescribingYourIssue) {
-//     removeAllActiveClass();
-//     step_one[0].classList.add("active");
-//   }
-// });
-
-// step_twoItem.addEventListener("click", function () {
-//   if (
-//     !!contactPageAllFormValues?.ChooseTheBestDescriptionOfYourIssue &&
-//     !!contactPageAllFormValues?.addAdditionalDetailsIfYouLikeOrNot
-//   ) {
-//     removeAllActiveClass();
-//     step_two[0].classList.add("active");
-//   }
-// });
-
-// step_threeItem.addEventListener("click", function () {
-//   if (
-//     !!contactPageAllFormValues?.email &&
-//     !!contactPageAllFormValues?.firstName &&
-//     !!contactPageAllFormValues?.lastName
-//   ) {
-//     removeAllActiveClass();
-//     step_three[0].classList.add("active");
-//   }
-// });
-
-// step_fourItem.addEventListener("click", function () {
-//   if (!!contactPageAllFormValues?.StartByDescribingYourIssue) {
-//     removeAllActiveClass();
-//     step_four[0].classList.add("active");
-//   }
-// });
-
-// arrayOfInput?.map((sInput, key)=> {
-//   sInput?.addEventListener("change", function(){
-
-//   })
-// })
-
 // here is text box style of input field
 
-// 1 input box
-allInput[0]?.addEventListener("input", (event) => {
-  contactPageAllFormValues["StartByDescribingYourIssue"] = event.target.value;
-  allInput[0].style.border = "1px solid #ddd";
-  document.querySelectorAll(
-    ".errorMessage_StartByDescribingYourIssue"
-  )[0].innerHTML = ` `;
-});
+// // 1 input box
+// allInput[0]?.addEventListener("input", (event) => {
+//   contactPageAllFormValues["StartByDescribingYourIssue"] = event.target.value;
+//   allInput[0].style.border = "1px solid #ddd";
+//   document.querySelectorAll(
+//     ".errorMessage_StartByDescribingYourIssue"
+//   )[0].innerHTML = ` `;
+// });
 
-allInput[0]?.addEventListener("click", function () {
-  label[0].style.top = "0";
-  label[0].style.color = "#049f8d";
-  label[0].style.background = "#fff";
-});
+// allInput[0]?.addEventListener("click", function () {
+//   label[0].style.top = "0";
+//   label[0].style.color = "#049f8d";
+//   label[0].style.background = "#fff";
+// });
 
-// 2 input box
-allInput[1]?.addEventListener("input", (event) => {
-  contactPageAllFormValues["addAdditionalDetailsIfYouLikeOrNot"] =
-    event.target.value;
-  allInput[1].style.border = "1px solid #ddd";
-  document.querySelectorAll(
-    ".errorMessage_addAdditionalDetailsIfYouLikeOrNot"
-  )[0].innerHTML = ` `;
-});
+// // 2 input box
+// allInput[1]?.addEventListener("input", (event) => {
+//   contactPageAllFormValues["addAdditionalDetailsIfYouLikeOrNot"] =
+//     event.target.value;
+//   allInput[1].style.border = "1px solid #ddd";
+//   document.querySelectorAll(
+//     ".errorMessage_addAdditionalDetailsIfYouLikeOrNot"
+//   )[0].innerHTML = ` `;
+// });
 
-allInput[1]?.addEventListener("click", function () {
-  label[1].style.top = "0";
-  label[1].style.color = "#049f8d";
-  label[1].style.background = "#fff";
-});
+// allInput[1]?.addEventListener("click", function () {
+//   label[1].style.top = "0";
+//   label[1].style.color = "#049f8d";
+//   label[1].style.background = "#fff";
+// });
 
-// 3 input box
-allInput[2]?.addEventListener("input", (event) => {
-  contactPageAllFormValues["email"] = event.target.value;
-  allInput[2].style.border = "1px solid #ddd";
-  document.querySelectorAll(".errorMessage_email")[0].innerHTML = ` `;
-});
+// // 3 input box
+// allInput[2]?.addEventListener("input", (event) => {
+//   contactPageAllFormValues["email"] = event.target.value;
+//   allInput[2].style.border = "1px solid #ddd";
+//   document.querySelectorAll(".errorMessage_email")[0].innerHTML = ` `;
+// });
 
-allInput[2]?.addEventListener("click", function () {
-  label[2].style.top = "0";
-  label[2].style.color = "#049f8d";
-  label[2].style.background = "#fff";
-});
+// allInput[2]?.addEventListener("click", function () {
+//   label[2].style.top = "0";
+//   label[2].style.color = "#049f8d";
+//   label[2].style.background = "#fff";
+// });
 
-// 4 input box
-allInput[3]?.addEventListener("input", (event) => {
-  contactPageAllFormValues["firstName"] = event.target.value;
-  allInput[3].style.border = "1px solid #ddd";
-  document.querySelectorAll(".errorMessage_firstName")[0].innerHTML = ` `;
-});
+// // 4 input box
+// allInput[3]?.addEventListener("input", (event) => {
+//   contactPageAllFormValues["firstName"] = event.target.value;
+//   allInput[3].style.border = "1px solid #ddd";
+//   document.querySelectorAll(".errorMessage_firstName")[0].innerHTML = ` `;
+// });
 
-allInput[3]?.addEventListener("click", function () {
-  label[3].style.top = "0";
-  label[3].style.color = "#049f8d";
-  label[3].style.background = "#fff";
-});
+// allInput[3]?.addEventListener("click", function () {
+//   label[3].style.top = "0";
+//   label[3].style.color = "#049f8d";
+//   label[3].style.background = "#fff";
+// });
 
-// 5 input box
-allInput[4]?.addEventListener("input", (event) => {
-  contactPageAllFormValues["lastName"] = event.target.value;
-  allInput[4].style.border = "1px solid #ddd";
-  document.querySelectorAll(".errorMessage_lastName")[0].innerHTML = ` `;
-});
+// // 5 input box
+// allInput[4]?.addEventListener("input", (event) => {
+//   contactPageAllFormValues["lastName"] = event.target.value;
+//   allInput[4].style.border = "1px solid #ddd";
+//   document.querySelectorAll(".errorMessage_lastName")[0].innerHTML = ` `;
+// });
 
-allInput[4]?.addEventListener("click", function () {
-  label[4].style.top = "0";
-  label[4].style.color = "#049f8d";
-  label[4].style.background = "#fff";
-});
+// allInput[4]?.addEventListener("click", function () {
+//   label[4].style.top = "0";
+//   label[4].style.color = "#049f8d";
+//   label[4].style.background = "#fff";
+// });
 
-// =================== contact page javascript codes is end =========================
+
+
+// input box validation with focus & blur & input value change
+input_groups?.forEach((element) => {
+  let InputBox = element.querySelector(".input");
+  let labelTag = element.querySelector("label");
+  let errorDiv = element.querySelector(".errorDiv");
+
+  InputBox?.addEventListener("focus", function () {
+    labelTag.style.top = "0";
+    labelTag.style.color = "#049f8d";
+    labelTag.style.background = "#fff";
+    return;
+  });
+
+  InputBox?.addEventListener("blur", function (e) {
+    if (!!contactPageAllFormValues[InputBox?.getAttribute("name")]) {
+      labelTag.style.top = "0";
+      labelTag.style.color = "#049f8d";
+      labelTag.style.background = "#fff";
+      errorDiv.innerHTML = ``;
+    } else {
+      InputBox.style.border = "1px solid #b00020";
+      labelTag.style.color = "#b00020";
+      labelTag.style.top = "25px";
+
+      errorDiv.innerHTML = `<p class="error">${InputBox?.getAttribute(
+        "errorMessage"
+      )}</p>`;
+    }
+  });
+
+  // validation when input box is change text
+  InputBox?.addEventListener("input", function (e) {
+    let value = e.target.value;
+    contactPageAllFormValues[InputBox?.getAttribute("name")] = value;
+
+    if (!!value) {
+      errorDiv.innerHTML = ``;
+      InputBox.style.border = "1px solid #ddd";
+    } else {
+      InputBox.style.border = "1px solid #b00020";
+      errorDiv.innerHTML = `<p class="error">${InputBox?.getAttribute(
+        "errorMessage"
+      )}</p>`;
+    }
+  });
+});
 
